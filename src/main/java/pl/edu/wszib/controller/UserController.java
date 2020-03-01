0e54e0a59e75c2cf37219bf3bf7794dce9cb601e
krdudeka <krdudeka@wszib.edu.pl>
2020-03-01 08:40:44 +0100

    Dokończenie zadania z zajęć

--- src/main/java/pl/edu/wszib/controller/UserController.java
+++ src/main/java/pl/edu/wszib/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.edu.wszib.dao.UserDao;
 import pl.edu.wszib.domain.User;
 
@@ -32,5 +33,17 @@ public class UserController {
         return "user";
     }
 
+    @PostMapping("users/save")
+    public String saveUser(User user) {
+        userDao.saveUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("users/edit/{id}")
+    public String editUser(@PathVariable Long id, Model model) {
+        User user = userDao.getById(id);
+        model.addAttribute("user", user);
+        return "user";
+    }
 
 }
